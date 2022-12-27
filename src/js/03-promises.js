3b18e9322616622ff2166e4aef4200721ce00a3f
Nazarenko Alex <nazarenko.alx@gmail.com>
2022-12-27 02:23:56 +0200

    updates task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const amount = Number(evt.currentTarget.elements.amount.value);
+  let delay = Number(evt.currentTarget.elements.delay.value);
+  const step = Number(evt.currentTarget.elements.step.value);
+
+  for (let i = 0; i <= amount; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) =>
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        )
+      )
+      .catch(({ position, delay }) =>
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`)
+      );
   }
+  delay += step;
 }
