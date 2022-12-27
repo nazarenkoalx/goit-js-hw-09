d3eb3ed176cfe4b796501746089fdf0a73b3eae1
nazarenkoalx <109482973+nazarenkoalx@users.noreply.github.com>
2022-12-27 00:24:27 +0000

    Deploying to gh-pages from @ nazarenkoalx/goit-js-hw-09@3b18e9322616622ff2166e4aef4200721ce00a3f 🚀

---
+++ 01-color-switcher.2aa5a517.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.querySelector("body");let o=null;function d(){r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}t.addEventListener("click",(function(){o=setInterval(d,1e3),t.setAttribute("disabled",!0),e.removeAttribute("disabled")})),e.addEventListener("click",(function(){clearInterval(o),t.removeAttribute("disabled"),e.setAttribute("disabled",!0)})),e.setAttribute("disabled",!0);
+//# sourceMappingURL=01-color-switcher.2aa5a517.js.map
